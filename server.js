ae27f24adc31b4cc3b52ac6125dbcfdd5c1a065b
unknown <abhirammoothedam@gmail.com>
2025-05-14 13:16:04 +0530

    resolved

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ app.get('/api/dashboard', (req, res) => {
 const authRoutes = require('./routes/auth');
 app.use('/api', authRoutes);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
